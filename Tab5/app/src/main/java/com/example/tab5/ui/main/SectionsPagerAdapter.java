c22898c7d45ffbff0f74cb59b6b83ffd14cd4d42
YG0122 <yoonkyong0122@gmail.com>
2019-06-27 14:01:33 +0900

    Tab
    Initial Tab

---
+++ Tab5/app/src/main/java/com/example/tab5/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.tab5.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import com.example.tab5.ui.main.fragments.Tab1;
+import com.example.tab5.ui.main.fragments.Tab2;
+import com.example.tab5.ui.main.fragments.Tab3;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    /*
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+    */
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        //mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        /*return PlaceholderFragment.newInstance(position + 1);*/
+        switch (position) {
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+            default:
+                return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        //return mContext.getResources().getString(TAB_TITLES[position]);
+        switch (position) {
+            case 0:
+                return "Tab 1";
+            case 1:
+                return "Tab 2";
+            case 2:
+                return "Tab 3";
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
---
+++ Tab5/app/src/main/java/com/example/tab5/ui/main/fragments/Tab3.java
@@ -0,0 +1,21 @@
+package com.example.tab5.ui.main.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tab5.R;
+
+public class Tab3 extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+
+        return rootView;
+    }
+}
